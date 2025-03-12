20196daf433b6d80a01aee1be6041b10b93e655d
Edson Alves <edsonapf@gmail.com>
2025-03-11 21:39:58 -0300

    feat: add new logic to generate phrases

--- src/index.ts
+++ src/index.ts
@@ -24,36 +24,52 @@ dbClient.connect();
 
 server.use(bodyParser.json());
 server.use(cors());
+// [].sort(() => Math.random() - 0.5)
+// https://www.freecodecamp.org/news/how-to-shuffle-an-array-of-items-using-javascript-or-typescript/
 // const phrases = [
-//   "Luiz/trabalha/na cidade/onde/vivem/seus avós",
-//   "Carol/fez/sua festa/onde/moram/seus pais",
-//   "Pedro/fez/sua tarefa/onde/pediu/ajuda a Mika",
-//   "Minha|mãe|é|muito|bonita",
-//   "Minha|colega|de sala|é|muito|inteligente",
-//   "Camila/saiu/de Cuba/onde/viveu/sete anos de vida",
-//   "O professor|de Sintaxe|é|legal",
-//   "Ele/foi/para casa/onde/mora/com sua família",
-//   "Maria/teve/uma infância/onde/foi/muito feliz",
-//   "Fui|ao cinema|ontem|com|meu namorado",
-//   "Eu|adoro|música|pop",
+//   "Luiz/trabalha/na cidade/onde/vivem/seus avós$pergunta?&sim*,não",
+//   "Carol/fez/sua festa/onde/moram/seus pais$pergunta?&sim*,não",
+//   "Pedro/fez/sua tarefa/onde/pediu/ajuda a Mika$pergunta?&sim*,não",
+//   "Minha|mãe|é|muito|bonita$pergunta?&sim*,não",
+//   "Minha|colega|de sala|é|muito|inteligente$pergunta?&sim*,não",
+//   "Camila/saiu/de Cuba/onde/viveu/sete anos de vida$pergunta?&sim*,não",
+//   "O professor|de Sintaxe|é|legal$pergunta?&sim*,não",
+//   "Ele/foi/para casa/onde/mora/com sua família$pergunta?&sim*,não",
+//   "Maria/teve/uma infância/onde/foi/muito feliz$pergunta?&sim*,não",
+//   "Fui|ao cinema|ontem|com|meu namorado$pergunta?&sim*,não",
+//   "Eu|adoro|música|pop$pergunta?&sim*,não",
 // ];
 server.get("/", (_, res) => {
-  // const fileContent = phrases.map((phrase) => {
-  //   const isFake = phrase.includes("|");
+  // const fileContent = phrases.map((item) => {
+  //   const isFake = item.includes("|");
+  //   const [phrase, questionAndAnswers] = item.split("$");
   //   const splittedPhrase = isFake ? phrase.split("|") : phrase.split("/");
   //   const stimulus = splittedPhrase.map((stimulu) => ({
-  //     type: "HtmlKeyboardResponsePlugin",
+  //     type: HtmlKeyboardResponsePlugin,
   //     stimulus: stimulu,
-  //     choices: " ",
+  //     choices: SPACE,
   //   }));
+  //   const [question, answers] = questionAndAnswers.split("&");
+  //   const [firstAnswer, secondAnswer] = answers.split(",");
 
   //   return {
   //     stimulus,
   //     fake: isFake,
   //     phrase: phrase.replace(/\/|\|/g, " "),
+  //     question,
+  //     answers: [
+  //       {
+  //         answer: firstAnswer.replace("*", ""),
+  //         isCorrect: firstAnswer.includes("*"),
+  //       },
+  //       {
+  //         answer: secondAnswer.replace("*", ""),
+  //         isCorrect: secondAnswer.includes("*"),
+  //       },
+  //     ],
   //   };
   // });
-  // fs.writeFileSync("experiments.json", JSON.stringify(fileContent));
+  // fs.writeFileSync("experiments.txt", JSON.stringify(fileContent));
   res.send("Tcc Laura");
 });
 
